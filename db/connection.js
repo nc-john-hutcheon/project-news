b6480da482fa0eef24e3a270d3312df5f59c02ef
nc-john-hutcheon <john.hutcheon@northcoders.com>
2024-12-16 09:31:04 +0000

    hosting liliias backend

--- db/connection.js
+++ db/connection.js
@@ -12,23 +12,22 @@
 
 // module.exports = new Pool();
 
-const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
 
-require('dotenv').config({
+const config = {};
+
+require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-  if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-    throw new Error("PGDATABASE or DATABASE_URL not set");
-  }
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
 
-  const config = {};
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
+}
 
-  if (ENV === "production") {
-    config.connectionString = process.env.DATABASE_URL;
-    config.max = 2;
-  }
-
-
-module.exports = new Pool( config );
+module.exports = new Pool(config);
